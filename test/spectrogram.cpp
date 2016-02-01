3da476d1f1ca45e3f103f0aadcd83c91ee3d17ad
Martin Guy <martinwguy@gmail.com>
2016-02-01 17:56:40 +0100

    Output PNG the right way up, add -d dynRange flag, normalize output in dB
    - Store data as it's calculated column by column then output it row by row;
    - Normalise the output by accumulating the maximum amplitude while calculating,
      then adjust the brightness so that the loudest pixel is white;
    - Convert output into decibels (it was magnitude);
    - Add -d flag to specify dynamic range of output.

--- test/spectrogram.cpp
+++ test/spectrogram.cpp
@@ -19,32 +19,42 @@ using std::endl;
 #include <sys/time.h>
 #include <cstdlib>
 
-static void output_rows(png_structp png_ptr,
-			png_uint_32 png_width,
-			CQSpectrogram::RealBlock rb,
-			double maxAmp);
+static void output_frames(CQSpectrogram::RealBlock rb);
+static void write_png(char *fileNameOut);
 
-static void show_min_max(void);
+// Number of data items in each analysis frame
+static unsigned int nbuckets;
+// Total number of frames to write to output file
+static unsigned int nframes;
 
-// How many of the initial output columns do we still need to ignore
+// How many of the initial output frames do we still need to ignore
 // to compensate for the latency?
-static png_uint_32 columnsToSkip;
-// How many more columns do we need to output into the PNG file?
-static png_uint_32 columnsToOutput;
+static png_uint_32 framesToSkip;
+// How many more frames do we need to output into the PNG file?
+static png_uint_32 framesToOutput;
+
+/* Data is generated column-by-column but PNGs need writing
+ * row-by-row so store output columns as they are generated
+ * then output them the right way up at the end.
+ * This also allows us to find the maximum amplitude and
+ * normalize the output so that the loudest pixel is white..
+ */
+static float **frames;	// All the output frames
+static float maxAmp = -1.0/0.0;	/* -infinity */
+static float dynRange = 100;	/* dynamic range of output in decibels */
 
 int main(int argc, char **argv)
 {
     double maxFreq = 0;
     double minFreq = 0;
-    double maxAmp = 1.0;
     double atomHopFactor = 0.0; /* If 0, not given as a parameter */
     int bpo = 0;
     bool help = false;
     CQSpectrogram::Interpolation interpolation = CQSpectrogram::InterpolateLinear;
     CQParameters::WindowType window = CQParameters::Hann;
 
-    // How many output columns have process() and getRemaining() returned to us?
-    int columnsReceived = 0;
+    // How many output frames have process() and getRemaining() returned to us?
+    int framesReceived = 0;
 
     int c;
 
@@ -55,7 +65,7 @@ int main(int argc, char **argv)
 	    { "help", 0, 0, 'h', },
 	    { "maxfreq", 1, 0, 'x', },
 	    { "minfreq", 1, 0, 'n', },
-	    { "maxamp", 1, 0, 'm', },
+	    { "dynrange", 1, 0, 'd', },
 	    { "atomhopfactor", 1, 0, 'a', },
 	    { "bpo", 1, 0, 'b' },
 	    { "interpolation", 1, 0, 'i' },
@@ -64,7 +74,7 @@ int main(int argc, char **argv)
 	};
 
 	c = getopt_long(argc, argv,
-			"hx:n:m:a:b:i:w:",
+			"hx:n:d:a:b:i:w:",
 			longOpts, &optionIndex);
 	if (c == -1) break;
 
@@ -72,7 +82,7 @@ int main(int argc, char **argv)
 	case 'h': help = true; break;
 	case 'x': maxFreq = atof(optarg); break;
 	case 'n': minFreq = atof(optarg); break;
-	case 'm': maxAmp = atof(optarg); break;
+	case 'd': dynRange = atof(optarg); break;
 	case 'a': atomHopFactor = atof(optarg); break;
 	case 'b': bpo = atoi(optarg); break;
 	case 'i': switch (optarg[0]) {
@@ -101,7 +111,7 @@ int main(int argc, char **argv)
 	cerr << "Options:" << endl;
 	cerr << "  -x<X>, --maxfreq <X>  Maximum frequency (default = sample rate / 3)" << endl;
 	cerr << "  -n<X>, --minfreq <X>  Minimum frequency (default = 100, actual min may vary)" << endl;
-	cerr << "  -m<X>, --maxamp <X>   Predited maximum output value (default = 1.0)" << endl;
+	cerr << "  -d<X>, --dynrange <X> Dynamic range of output in dB (default: 100)" << endl;
 	cerr << "  -a<X>, --atomhopfactor <X> Atom hop factor (default = 0.25)" << endl;
 	cerr << "  -b<X>, --bpo <X>      Bins per octave   (default = 60)" << endl;
 	cerr << "  -i<X>, --interpolation <X>" << endl;
@@ -144,60 +154,21 @@ int main(int argc, char **argv)
     if (atomHopFactor != 0.0) params.atomHopFactor = atomHopFactor;
     CQSpectrogram cq(params, interpolation);
 
-#if 0
-    cerr << "octave boundaries: ";
-    for (int i = 0; i < cq.getOctaves(); ++i) {
-        cerr << cq.getMaxFrequency() / pow(2, i) << " ";
-    }
-    cerr << endl;
-#endif
+    nbuckets = (png_uint_32) (log2 (maxFreq / minFreq) * bpo + 0.5);
+    nframes = sfinfo.frames / cq.getColumnHop();
 
-    png_structp png_ptr;
-    png_infop png_info;
-    png_uint_32 png_width;
-
-    /* Create PNG file for writing */
-    FILE *png_fp = fopen(fileNameOut, "wb");
-    if (!png_fp) {
-	cerr << "Cannot create " << fileNameOut << endl;
+    frames = new float * [nframes];
+    if (!frames) {
+	cerr << "Out of memory." << endl;
 	exit(1);
     }
-    png_ptr = png_create_write_struct(
-	PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png_ptr) {
-	cerr << "Cannot create PNG writer" << endl;
-	exit(1);
-    }
-    png_info = png_create_info_struct(png_ptr);
-    if (!png_info) {
-	cerr << "Cannot create PNG info" << endl;
-	exit(1);
-    }
-    if (setjmp(png_jmpbuf(png_ptr)))
-    {
-	cerr << "Something went wrong in the PNG-writing subsystem. Quitting..." << endl;
-	png_destroy_write_struct(&png_ptr, &png_info);
-        fclose(png_fp);
-	exit(1);
-    }
-    png_width = (png_uint_32) (log2 (maxFreq / minFreq) * bpo + 0.5);
-    png_uint_32 png_height = sfinfo.frames / cq.getColumnHop();
-    png_init_io(png_ptr, png_fp);
-    png_set_IHDR(png_ptr, png_info,
-	png_width, png_height,
-	8, // bit depth
-	PNG_COLOR_TYPE_GRAY,
-	PNG_INTERLACE_NONE,
-	PNG_COMPRESSION_TYPE_DEFAULT,
-	PNG_FILTER_TYPE_DEFAULT);
-    png_write_info(png_ptr, png_info);
 
     int inframe = 0;
 
     vector<double> buffer;
 
-    columnsToSkip = (png_uint_32)((double)cq.getLatency() / cq.getColumnHop() + 0.5);
-    columnsToOutput = png_height;
+    framesToSkip = (png_uint_32)((double)cq.getLatency() / cq.getColumnHop() + 0.5);
+    framesToOutput = nframes;
 
     while (inframe < sfinfo.frames) {
         int count = -1;
@@ -224,8 +195,8 @@ int main(int argc, char **argv)
 	// RealBlock is a vector of RealColumn's and
 	// RealColumn is a vector of double.
         if (rb.size() > 0) {
-	    columnsReceived += rb.size();
-	    output_rows(png_ptr, png_width, rb, maxAmp);
+	    framesReceived += rb.size();
+	    output_frames(rb);
 	}
 
 	inframe += count;
@@ -235,55 +206,108 @@ int main(int argc, char **argv)
 
     CQSpectrogram::RealBlock rb = cq.getRemainingOutput();
     if (rb.size() > 0) {
-	columnsReceived += rb.size();
-	output_rows(png_ptr, png_width, rb, maxAmp);
+	framesReceived += rb.size();
+	output_frames(rb);
     }
 
-    png_write_end(png_ptr, NULL);
-
-    show_min_max();
+    write_png(fileNameOut);
 
     return 0;
 }
 
-static double minval, maxval;
-static bool initval=0;
-
 static void
-show_min_max()
+output_frames(CQSpectrogram::RealBlock rb)
 {
-    cerr << "Maximum output value: " << maxval << endl;
+    static float **framesp = NULL;
+    if (framesp == NULL) framesp = frames;
+
+    for (unsigned i = 0; i < rb.size(); i++) {
+	if (framesToSkip > 0) {
+	    framesToSkip--;
+	    continue;
+        }
+        if (framesToOutput <= 0)
+	    return;
+
+        float *frame = new float[nbuckets];
+        if (!frame) {
+	    cerr << "Out of memory." << endl;
+	    exit(1);
+        }
+	for (unsigned j = 0; j < nbuckets; j++) {
+	    float value = 20.0 * log10(rb[i][j]);	// Convert to dB
+	    frame[j] = value;
+	    if (value > maxAmp) maxAmp=value;
+	}
+	*framesp++ = frame;
+
+        framesToOutput--;
+    }
 }
 
 static void
-output_rows(png_structp png_ptr, png_uint_32 png_width, CQSpectrogram::RealBlock rb, double maxAmp)
+write_png(char *fileNameOut)
 {
-    static png_bytep png_row = NULL;
-    if (png_row == NULL) png_row = (png_bytep) malloc(png_width);
+    //
+    // Write PNG header
+    //
+    png_structp png_ptr;
+    png_infop png_info;
+    png_uint_32 png_width = nframes;
+    png_uint_32 png_height = nbuckets;
+
+    /* Create PNG file for writing */
+    FILE *png_fp = fopen(fileNameOut, "wb");
+    if (!png_fp) {
+	cerr << "Cannot create " << fileNameOut << endl;
+	exit(1);
+    }
+    png_ptr = png_create_write_struct(
+	PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png_ptr) {
+	cerr << "Cannot create PNG writer" << endl;
+	exit(1);
+    }
+    png_info = png_create_info_struct(png_ptr);
+    if (!png_info) {
+	cerr << "Cannot create PNG info" << endl;
+	exit(1);
+    }
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+	cerr << "Something went wrong in the PNG-writing subsystem. Quitting..." << endl;
+	png_destroy_write_struct(&png_ptr, &png_info);
+        fclose(png_fp);
+	exit(1);
+    }
+    png_init_io(png_ptr, png_fp);
+    png_set_IHDR(png_ptr, png_info,
+	png_width, png_height,
+	8, // bit depth
+	PNG_COLOR_TYPE_GRAY,
+	PNG_INTERLACE_NONE,
+	PNG_COMPRESSION_TYPE_DEFAULT,
+	PNG_FILTER_TYPE_DEFAULT);
+    png_write_info(png_ptr, png_info);
+
+    //
+    // Write image
+    //
+    png_bytep png_row = new png_byte[png_width];
     if (!png_row) {
 	cerr << "Out of memory." << endl;
 	exit(1);
     }
 
-    if (!initval) minval = maxval = rb[0][0];
-
-    /* Convert doubles to 0-255 */
-    for (unsigned i = 0; i < rb.size(); i++) {
-	if (columnsToSkip > 0) {
-	    columnsToSkip--;
-	    continue;
-        }
-        if (columnsToOutput <= 0)
-	    return;
-	for (unsigned from = 0, to = png_width; to > 0; ) {
-	    double value = rb[i][from++];
-	    if (value < minval) minval=value;
-	    if (value > maxval) maxval=value;
+    for (png_uint_32 y=0; y < png_height; y++) {
+        for (png_uint_32 x=0; x < png_width; x++) {
+	    float value = (maxAmp - frames[x][y]) / dynRange;
 	    if (value < 0.0) { value = 0.0; }
-	    if (value > maxAmp ) { value = maxAmp; }
-	    png_row[--to] = (value / maxAmp * 255) + 0.5;
+	    if (value > 1.0) { value = 1.0; }
+	    png_row[x] = ((1.0-value) * 255) + 0.5;
 	}
 	png_write_row(png_ptr, png_row);
-	columnsToOutput--;
     }
+
+    png_write_end(png_ptr, NULL);
 }
